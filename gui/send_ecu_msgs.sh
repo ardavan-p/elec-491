0105fb45ba67eda365fafc23e70a13a12bd5f76f
Mihir Nimgade <mihir.nim@outlook.com>
2024-08-04 14:08:27 -0700

    Update bash script

--- gui/send_ecu_msgs.sh
+++ gui/send_ecu_msgs.sh
@@ -6,8 +6,15 @@ echo "log,info,test info message two" >> temp_serial_port
 
 # send a few `ptn` messages
 
+# ptn,ptn_id,sensor_id,pressure,temp,status_code,paired
+
 echo "ptn,1,85,2048,49,0,1" >> temp_serial_port
+sleep 4
 echo "ptn,2,170,1802,85,0,1" >> temp_serial_port
+sleep 10
+echo "ptn,2,170,1090,48,1,1" >> temp_serial_port
+sleep 5
+echo "ptn,2,170,1090,48,2,1" >> temp_serial_port
 
 # send a few `ecu` messages
 
