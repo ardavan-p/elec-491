355fe77fceb7c77833a539150d78cfbda436fb2b
Mihir Nimgade <mihir.nim@outlook.com>
2024-08-07 11:16:03 -0700

    Add reset message to test scripts

--- gui/send_ecu_msgs.sh
+++ gui/send_ecu_msgs.sh
@@ -4,14 +4,15 @@
 echo "log,info,test info message one" >> temp_serial_port
 echo "log,info,test info message two" >> temp_serial_port
 
-# send a few `ptn` messages
+# send the reset message
+echo "reset,reset,reset" >> temp_serial_port
 
 # ptn,ptn_id,sensor_id,pressure,temp,status_code,paired
 
 echo "ptn,1,85,2048,49,0,1" >> temp_serial_port
-sleep 4
+sleep 5
 echo "ptn,2,170,1802,85,0,1" >> temp_serial_port
-sleep 10
+sleep 5
 echo "ptn,2,170,1090,48,1,1" >> temp_serial_port
 sleep 5
 echo "ptn,2,170,1090,48,2,1" >> temp_serial_port
@@ -19,6 +20,9 @@ echo "ptn,2,170,1090,48,2,1" >> temp_serial_port
 # send a few `ecu` messages
 
 echo "ecu,0,reset state entered" >> temp_serial_port
+sleep 5
 echo "ecu,1,autolocalization started" >> temp_serial_port
+sleep 5
 echo "ecu,1,autolocalization finished" >> temp_serial_port
+sleep 5
 echo "ecu,2,regular system operation begin" >> temp_serial_port
